98522f4e863a14b93d4d27bca2a52ee0dcad74c9
Kaiquemcr <Kaiquedev404@gmail.com>
2022-03-04 23:09:57 -0300

    scroll animation replacement with lib

--- js/main.js
+++ js/main.js
@@ -1,9 +1,41 @@
-function onloadAnimation() {
-  document.querySelectorAll('img').forEach((img, index) => {
-    if (img.getBoundingClientRect().top < window.innerHeight) {
-      img.src = img.getAttribute('data-src')
-    }
-  })
-}
+// headings
 
-window.addEventListener('scroll', onloadAnimation)
+ScrollReveal().reveal('.headings', { scale: 0 })
+
+// Gallery images
+
+ScrollReveal().reveal('.gallery1', { scale: 0 })
+ScrollReveal().reveal('.gallery2', { scale: 0, delay: 100 })
+ScrollReveal().reveal('.gallery3', { scale: 0, delay: 200 })
+ScrollReveal().reveal('.gallery4', { scale: 0, delay: 300 })
+ScrollReveal().reveal('.gallery5', { scale: 0, delay: 0 })
+ScrollReveal().reveal('.gallery6', { scale: 0, delay: 100 })
+ScrollReveal().reveal('.gallery7', { scale: 0, delay: 300 })
+ScrollReveal().reveal('.gallery8', { scale: 0, delay: 400 })
+
+// Infos and numbers
+
+ScrollReveal().reveal('.number1', { scale: 0 })
+ScrollReveal().reveal('.number2', { scale: 0, delay: 100 })
+ScrollReveal().reveal('.number3', { scale: 0, delay: 200 })
+
+ScrollReveal().reveal('.info1', { scale: 0, delay: 300 })
+ScrollReveal().reveal('.info2', { scale: 0, delay: 400 })
+
+// Popular of the week
+
+ScrollReveal().reveal('.popular-title', { scale: 0 })
+ScrollReveal().reveal('.card1', { scale: 0, delay: 100 })
+ScrollReveal().reveal('.card2', { scale: 0, delay: 200 })
+ScrollReveal().reveal('.card3', { scale: 0, delay: 300 })
+ScrollReveal().reveal('.card4', { scale: 0, delay: 400 })
+
+// Best artists
+
+ScrollReveal().reveal('.best-title', { scale: 0 })
+ScrollReveal().reveal('.artist1', { scale: 0, delay: 100 })
+ScrollReveal().reveal('.artist2', { scale: 0, delay: 200 })
+ScrollReveal().reveal('.artist3', { scale: 0, delay: 300 })
+ScrollReveal().reveal('.artist4', { scale: 0, delay: 400 })
+ScrollReveal().reveal('.artist5', { scale: 0, delay: 500 })
+ScrollReveal().reveal('.artist6', { scale: 0, delay: 600 })
